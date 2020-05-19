9d6f01b27ac29efa5b062ceb44827b756af12624
IemProg <btoxic24@gmail.com>
2020-05-19 04:24:23 +0100

    Correct error in data augmentation

--- data_augmentation.py
+++ data_augmentation.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import math
 import numpy as np
 from skimage import morphology
+from skimage.transform import resize
 import scipy
 import random
 
@@ -12,16 +13,8 @@ image_len = 28
 image_wid = 28
 n_augmentation = 50
 
-
 def rescale_down_sample(img, new_len, new_wid):
-    length, width = img.shape
-    factor_len, remainder_len = divmod(length, new_len)
-    factor_wid, remainder_wid = divmod(width, new_wid)
-    off_set_len_inf = math.floor((length-(new_len*factor_len))/2)
-    off_set_len_sup = math.ceil((length-(new_len*factor_len))/2)
-    off_set_wid_inf = math.floor((width-(new_wid*factor_wid))/2)
-    off_set_wid_sup = math.ceil((width-(new_wid*factor_wid))/2)
-    img = img[off_set_len_inf:length-off_set_len_sup:factor_len,off_set_wid_inf:width - off_set_wid_sup:factor_wid]
+    img = resize(img, (new_len, new_wid))
     assert img.shape == (new_len,new_wid), "Image is " + str(img.shape)
     return img
 
@@ -53,8 +46,8 @@ def translate(img, dx, dy):
     x_moved = np.roll(img, dx, 1)
     return np.roll(x_moved, dy, 0)
 
-def apply_all(img, theta, unzoom, dx, dy):
-    return translate(unzoom_image(rotate_image(img, theta),unzoom),dx,dy)
+def apply_all(img, theta, unzoom, dx, dy, image_len, image_wid):
+    return resize(translate(unzoom_image(rotate_image(img, theta),unzoom),dx,dy), (image_len, image_wid))
 
 path = "operators/"
 
@@ -86,11 +79,11 @@ def generate_data(path = "operators/", image_len = 28, image_wid = 28, n_augment
         unzoom = random.uniform(0.5, 1) if random.uniform(0,1) > 0.2 else 1
         dx = random.randint(1, 5) if random.uniform(0,1) > 0.2 else 1
         dy = random.randint(1, 5) if random.uniform(0,1) > 0.2 else 1
-        augmented_plus = apply_all(plus_image_ds,theta, unzoom, dx, dy)
-        augmented_minus = apply_all(minus_image_ds,theta, unzoom, dx, dy)
-        augmented_multiply = apply_all(multiply_image_ds,theta, unzoom, dx, dy)
-        augmented_divide = apply_all(divide_image_ds,theta, unzoom, dx, dy)
-        augmented_equal = apply_all(equal_image_ds,theta, unzoom, dx, dy)
+        augmented_plus = apply_all(plus_image_ds,theta, unzoom, dx, dy, image_len, image_wid)
+        augmented_minus = apply_all(minus_image_ds,theta, unzoom, dx, dy, image_len, image_wid)
+        augmented_multiply = apply_all(multiply_image_ds,theta, unzoom, dx, dy, image_len, image_wid)
+        augmented_divide = apply_all(divide_image_ds,theta, unzoom, dx, dy, image_len, image_wid)
+        augmented_equal = apply_all(equal_image_ds,theta, unzoom, dx, dy, image_len, image_wid)
         augmented_data_set['+'].append(augmented_plus)
         augmented_data_set['-'].append(augmented_minus)
         augmented_data_set['*'].append(augmented_multiply)
